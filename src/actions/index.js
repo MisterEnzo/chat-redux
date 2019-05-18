963a487b0db4c154ee75c9af28227c88490e9857
enz <enzo_naguiat@yahoo.com>
2019-05-18 16:02:33 +0900

    refactor fetchMessage action to retrieve messages from lewagon chat api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,40 +1,20 @@
 import * as types from './action_types';
 
-// export function fetchMessagesSuccess(messages) {
-//   return { type: types.FETCH_MESSAGES, messages: [
-//     {
-//       "author":"anonymous92",
-//       "content":"payload",
-//       "created_at":"2017-09-26T23:03:16.365Z"
-//     }
-//   ]}
-// }
-
 export function fetchMessages(channel) {
-  // fetch('https://wagon-chat.herokuapp.com/general/messages')
-  // .then(response => response.json())
-  // .then(data => {
-    return { type: types.FETCH_MESSAGES, messages: [
-      {
-        "author":"anonymous92",
-        "content":"payload",
-        "created_at":"2017-09-26T23:03:16.365Z"
-      }
-    ]}
-  // })
-  // return function(dispatch) {
-  //   return fetch('https://wagon-chat.herokuapp.com/general/messages')
-  //   .then(response => response.json())
-  //   .then(data => {
-  //    dispatch(fetchMessagesSuccess(data.channel))
-  //   })
-    // .then(data => return {type: types.};
-    // return {type: types.FETCH_MESSAGES, payload: [
-    //   {
-    //     "author":"anonymous92",
-    //     "content":"payload",
-    //     "created_at":"2017-09-26T23:03:16.365Z"
-    //   }
-    // ]}
-  // };
+  // return { type: types.FETCH_MESSAGES, messages: [
+  //   {
+  //     "author":"anonymous92",
+  //     "content":"payload",
+  //     "created_at":"2017-09-26T23:03:16.365Z"
+  //   }
+  // ]}
+  // ---
+  return fetch('https://wagon-chat.herokuapp.com/general/messages')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    return { type: types.FETCH_MESSAGES, messages: data.messages}
+  })
+
 }
