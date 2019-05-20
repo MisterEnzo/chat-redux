35eacce4be2e119ea7e87a637c5bbb6291a091d7
enz <enzo_naguiat@yahoo.com>
2019-05-20 23:11:56 +0900

    refactor MessageBar component to a class instead of a functional component. add form, submit button, and state for input form

--- src/components/message_bar.jsx
+++ src/components/message_bar.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MessageBar = (props) => {
-  return (
-    <div>Message Bar Button</div>
-  );
+class MessageBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      message: ''
+    };
+  }
+
+  onFormChange = (message) => {
+    this.setState({
+      message: message
+    })
+  }
+  render() {
+    return (
+      <div>
+        <div>Message Bar Button</div>
+        <input type='text' value={this.state.message} onChange={(event) => this.onFormChange(event.target.value)} />
+        <input type='submit' value='Submit' className='btn'/>
+      </div>
+    );
+  }
+
 };
 
 export default MessageBar;
